26dd84cf0f36174ba60861c25af9c5c69c934bfa
Victor Landim <victorlandim5@gmail.com>
2019-10-22 19:37:00 -0300

    lemmatizer.py updated.

--- lemmatizer.py
+++ lemmatizer.py
@@ -13,25 +13,14 @@ start_time = time.time()
 
 cogroo = Cogroo.Instance()
 
-client = MongoClient('localhost', 27017)
-db = client["discursoDB"]
-discursos = db["discursos"]
-
-discursos_raw = discursos.find({"Conteudo": {
-    "$ne": ""
-}}, {"Conteudo": 1, "_id": 1})
-
-# discursos_raw = discursos_raw[0:10]  # remove
+with open('discursos_raw.pickle', 'rb') as handle:
+    discursos_raw = pickle.load(handle)
 
 discursos_list = []
-discursos_ids = []
 
 for disc in discursos_raw:
     text = disc["Conteudo"]
     discursos_list.append(text)
-
-    text_id = str(disc["_id"])
-    discursos_ids.append(text_id)
 
 discursos_lemmatized = []
 
@@ -53,12 +42,6 @@ discursos_processed = pipe.transform(discursos_lemmatized)
 
 with open("discursos_lemma.pickle", 'wb') as handle:
     pickle.dump(discursos_processed, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-with open("discursos_lemma_ids.pickle", 'wb') as handle:
-    pickle.dump(discursos_ids, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-# with open('discursos_lemma.pickle', 'rb') as handle:
-#     b = pickle.load(handle)
 
 elapsed_time = time.time() - start_time
 print(time.strftime("Discursos pre processados, lemmatizados e salvos, demorou %H:%M:%S:%m",
